cadc158726add5c88772a071025da0c735eb92ba
Greg Armstrong <12462033+gdarmstrong@users.noreply.github.com>
2020-09-09 10:50:12 -0600

    Update schema_privileges.sql

--- schema_privileges.sql
+++ schema_privileges.sql
@@ -19,3 +19,34 @@ GRANT USAGE ON schema events TO <usename>
 
 -- Grant Select on table
 GRANT SELECT ON table reports.active_subscriptions_detail_v TO <usename>
+
+-- View Table Privileges
+SELECT * 
+FROM 
+    (
+    SELECT 
+        schemaname
+        ,objectname
+        ,usename
+        ,HAS_TABLE_PRIVILEGE(usrs.usename, fullobj, 'select') AND has_schema_privilege(usrs.usename, schemaname, 'usage')  AS sel
+        ,HAS_TABLE_PRIVILEGE(usrs.usename, fullobj, 'insert') AND has_schema_privilege(usrs.usename, schemaname, 'usage')  AS ins
+        ,HAS_TABLE_PRIVILEGE(usrs.usename, fullobj, 'update') AND has_schema_privilege(usrs.usename, schemaname, 'usage')  AS upd
+        ,HAS_TABLE_PRIVILEGE(usrs.usename, fullobj, 'delete') AND has_schema_privilege(usrs.usename, schemaname, 'usage')  AS del
+        ,HAS_TABLE_PRIVILEGE(usrs.usename, fullobj, 'references') AND has_schema_privilege(usrs.usename, schemaname, 'usage')  AS ref
+    FROM
+        (
+        SELECT schemaname, 't' AS obj_type, tablename AS objectname, schemaname + '.' + tablename AS fullobj FROM pg_tables
+        WHERE schemaname not in ('pg_internal')
+        UNION
+        SELECT schemaname, 'v' AS obj_type, viewname AS objectname, schemaname + '.' + viewname AS fullobj FROM pg_views
+        WHERE schemaname not in ('pg_internal')
+        ) AS objs
+        ,(SELECT * FROM pg_user) AS usrs
+    ORDER BY fullobj
+    )
+WHERE (sel = true or ins = true or upd = true or del = true or ref = true)
+--and schemaname='<opt schema>'
+and usename = 'jeffreyyu';
+
+-- Grant Select on Schema
+GRANT SELECT ON ALL TABLES IN SCHEMA events TO jeffreyyu
